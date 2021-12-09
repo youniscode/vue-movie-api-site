64a4fa4def271154eec5cfe949b1ccededca6174
younis <lucas.mahler.mw@gmail.com>
2021-12-09 17:17:02 +0100

    finishedHomepage&AddedFirstMove

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,6 +18,11 @@
         </div>
       </router-link>
     </div>
+    <form @submit.prevent="" class="search-box">
+      <input type="text" placeholder="What are you looking for?" />
+      <input type="submit" value="Search" />
+    </form>
+    <div class="movies-list">Movies</div>
   </div>
 </template>
 
@@ -62,6 +67,54 @@ export default {};
       }
       p {
         color: white;
+      }
+    }
+  }
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+
+    input {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      &[type="text"] {
+        width: 100%;
+        color: #fff;
+        background-color: #496583;
+        font-size: 20px;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
+
+        &::placeholder {
+          color: #f3f3f3;
+        }
+        // give box shadow when hover over the search bar
+        &:focus {
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+        }
+      }
+      &[type="submit"] {
+        width: 100%;
+        max-width: 300px;
+        background-color: #42b883;
+        padding: 16px;
+        border-radius: 8px;
+        color: #fff;
+        text-transform: uppercase;
+        transition: 0.4s;
+
+        &:active {
+          background-color: #3b8070;
+        }
       }
     }
   }
