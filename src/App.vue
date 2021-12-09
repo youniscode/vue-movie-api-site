708f9695faec3156346f29e13c98dbb16ec9a817
younis <lucas.mahler.mw@gmail.com>
2021-12-09 10:52:43 +0100

    initializeMovieVueApiStarter

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,46 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header>
+    <router-link to="/">
+      <h1>Vue<span></span>Movies</h1>
+    </router-link>
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer></footer>
+  <router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
+
+
+
+
+
+
+
+
+
+
+
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Fira Sans", sans-serif;
+
+  &::selection {
+    background: transparentize(#42b883, 0.5);
+  }
+
+  body {
+    background-color: #35495e;
+  }
 
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+    text-decoration: none;
   }
 }
 </style>
---
+++ src/env.js
@@ -0,0 +1,3 @@
+export default {
+  apikey: "3ede5bd8"
+}
